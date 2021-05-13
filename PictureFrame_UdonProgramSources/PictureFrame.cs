a724862beea8a12506c01a234bd8d140e70dd9b3
Pachy <9065077+kdingo@users.noreply.github.com>
2021-05-13 01:04:17 -0700

    shuffle mode replacing random mode

--- PictureFrame_UdonProgramSources/PictureFrame.cs
+++ PictureFrame_UdonProgramSources/PictureFrame.cs
@@ -5,55 +5,70 @@ using VRC.Udon;
 
 public class PictureFrame : UdonSharpBehaviour {
     public Texture[] pictureList;
-    public bool randomOrder = false;
+    private int[] shuffleOrder;
+    public bool shufflePictures = false;
     public float pictureInterval = 15f;
     private float pictureIntervalTime = 0;
     private readonly string screenName = "Screen";
     private MeshRenderer screenRenderer;
-    private int pictureIndex = -1;
-    [UdonSynced] private int sync_pictureIndex = -1;
+    private int pictureIndex = 0;
+    private int pictureCount = -1;
+    [UdonSynced] private int sync_pictureIndex = 0;
 
     void Start() {
         screenRenderer = gameObject.transform.Find(screenName).GetComponent<MeshRenderer>();
+        shuffleOrder = ShuffleList(pictureList);
         if (Networking.IsMaster) {
             pictureIntervalTime = Time.time + pictureInterval;
-            pictureChange();
+            PictureChange();
         }
     }
 
     private void Update() {
         if (Time.time >= pictureIntervalTime && Networking.IsMaster) {
-            pictureChange();
+            PictureChange();
         }
     }
 
-    public void pictureChange() {
-        pictureIndex = nextPictureIndex();
-        sync_pictureIndex = pictureIndex;
-        screenRenderer.material.mainTexture = pictureList[pictureIndex];
-        pictureIntervalTime = Time.time + pictureInterval;
-    }
-
-    private int nextPictureIndex() {
-        if (randomOrder) {
-            return Random.Range(0, pictureList.Length);
+    public void PictureChange() {
+        if(++pictureCount >= pictureList.Length) {
+            pictureCount = 0;
+        }
+        if (shufflePictures) {
+            pictureIndex = shuffleOrder[pictureCount];
         }
         else {
-            if(++pictureIndex >= pictureList.Length) {
-                pictureIndex = 0;
-            }
+            pictureIndex = pictureCount;
         }
-        return pictureIndex;
+        screenRenderer.material.mainTexture = pictureList[pictureIndex];
+        sync_pictureIndex = pictureIndex;
+        pictureIntervalTime = Time.time + pictureInterval;
     }
 
     public override void OnDeserialization() {
         if (sync_pictureIndex != pictureIndex) {
-            pictureChangeDeserialize();
+            PictureChangeDeserialize();
         }
     }
 
-    private void pictureChangeDeserialize() {
+    private void PictureChangeDeserialize() {
         pictureIndex = sync_pictureIndex;
         screenRenderer.material.mainTexture = pictureList[pictureIndex];
+    }
+
+    private int[] ShuffleList(Texture[] textures) {
+        int[] shuffle = new int[textures.Length];
+        for (int x = 0; x < textures.Length; x++) {
+            shuffle[x] = x;
+        }
+
+        int temp;
+        for (int i = 0; i < shuffle.Length - 2; i++) {
+            temp = shuffle[i];
+            int j = Random.Range(i, shuffle.Length);
+            shuffle[i] = shuffle[j];
+            shuffle[j] = temp;
+        }
+        return shuffle;
     }
 }
