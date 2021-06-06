6fe4c0dc4b543c1f9c34f15f51e0f6c126e7c590
Pachy <9065077+kdingo@users.noreply.github.com>
2021-06-05 21:47:09 -0700

    github and booth release

--- PictureFrame_UdonProgramSources/PictureFrame.cs
+++ PictureFrame_UdonProgramSources/PictureFrame.cs
@@ -1,4 +1,8 @@
-﻿using UdonSharp;
+﻿/* Pachy Picture Frame
+ * by pachipon@VRC v20210518
+ * ©2021 licensed under CC BY-SA
+ */
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -8,15 +12,17 @@ public class PictureFrame : UdonSharpBehaviour {
     private int[] shuffleOrder;
     public bool shufflePictures = false;
     public float pictureInterval = 15f;
+    public Material screenMaterial;
     private float pictureIntervalTime = 0;
-    private readonly string screenName = "Screen";
     private MeshRenderer screenRenderer;
+    private string screenName;
     private int pictureIndex = 0;
     private int pictureCount = -1;
     [UdonSynced] private int sync_pictureIndex = 0;
 
     void Start() {
-        screenRenderer = gameObject.transform.Find(screenName).GetComponent<MeshRenderer>();
+        screenName = screenMaterial.name;
+        screenRenderer = gameObject.GetComponent<MeshRenderer>();
         shuffleOrder = ShuffleList(pictureList);
         if (Networking.IsMaster) {
             pictureIntervalTime = Time.time + pictureInterval;
@@ -30,6 +36,9 @@ public class PictureFrame : UdonSharpBehaviour {
         }
     }
 
+    /// <summary>
+    /// Changes the displayed picture. Must be called by World Master only
+    /// </summary>
     public void PictureChange() {
         if(++pictureCount >= pictureList.Length) {
             pictureCount = 0;
@@ -40,25 +49,52 @@ public class PictureFrame : UdonSharpBehaviour {
         else {
             pictureIndex = pictureCount;
         }
-        screenRenderer.material.mainTexture = pictureList[pictureIndex];
         sync_pictureIndex = pictureIndex;
+        Material m = GetScreenMaterial();
+        if (m) { m.mainTexture = pictureList[pictureIndex]; }
         pictureIntervalTime = Time.time + pictureInterval;
     }
 
+    /// <summary>
+    /// Cycle through the renderer's materials and get the material for the picture display. Must match with the material set as "Picture Material"
+    /// </summary>
+    /// <returns>Picture display material</returns>
+    private Material GetScreenMaterial() {
+        Material[] rendererMaterials = screenRenderer.materials;
+        foreach (Material mat in rendererMaterials) {
+            if (mat.name.Equals(screenName + " (Instance)")) {
+                return mat;
+            }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Check whenever the synced variable is changed by the master
+    /// </summary>
     public override void OnDeserialization() {
         if (sync_pictureIndex != pictureIndex) {
             PictureChangeDeserialize();
         }
     }
 
+    /// <summary>
+    /// All non-masters will change the picture after the master changes the picture first
+    /// </summary>
     private void PictureChangeDeserialize() {
         pictureIndex = sync_pictureIndex;
-        screenRenderer.material.mainTexture = pictureList[pictureIndex];
+        Material m = GetScreenMaterial();
+        if (m) { m.mainTexture = pictureList[pictureIndex]; }
     }
 
+    /// <summary>
+    /// Shuffle the picture list for a shuffled display.
+    /// </summary>
+    /// <param name="textures">The array of textures to shuffle</param>
+    /// <returns>An array of shuffled textures</returns>
     private int[] ShuffleList(Texture[] textures) {
         int[] shuffle = new int[textures.Length];
-        for (int x = 0; x < textures.Length; x++) {
+        for (int x = 0; x < shuffle.Length; x++) {
             shuffle[x] = x;
         }
 
--- Readme.md
+++ Readme.md
@@ -1,6 +1,46 @@
 # Pachy Picture Frame
 by pachipon@VRC
+https://pachipawnshop.booth.pm/
 A picture slide show made in UdonSharp. It is meant to be used in a VRChat SDK3 world.
 
+## Features
+ - Ordered and shuffle options
+ - Picture sync between players
+ - Multiple picture frames in one world supported
+
+## Installation
+ - Grab the Prefab and place it into your scene.
+	 - You may scale the prefab on the X and Y axes
+ - Locate the prefab script settings under the Udon Behavior component
+ - Set the Picture List size
+ - Add pictures to the list
+ - If you want a randomized list per instance, select Shuffle List
+ - Set the time between pictures
+ - Done!
+
+## Quirks
+Picture Material in the Mesh Renderer and Screen Material in the Udon Behavior script must be the same.
+There is no aspect ratio lock or auto resizing of different orientations. This prefab comes with landscape and portrait orientations in a 16:9 ratio
+
+## Changelog
+- 2021/06/04 first release (VRCSDK3 2021.04.21.11.57)
+
+## Contact
+ - @kdingo on Twitter
+ - pachipon on VRC
+
+## Credit
+
+[Shuffle algorithm](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+[Source code on Github](https://github.com/kdingo)
+
+## Thanks to
+
+My wonderful VRC friends
+
+## License
+This work is licensed under Creative Commons BY-SA
+- EN: https://creativecommons.org/licenses/by-sa/4.0/
+- JP: https://creativecommons.org/licenses/by-sa/4.0/deed.ja
 
 > Written with [StackEdit](https://stackedit.io/).
